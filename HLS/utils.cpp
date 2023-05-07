832c583bc40ec9a41f6ff0a3d6801640d06b9c21
Devansh Johri <devansh1996@gmail.com>
2023-05-07 01:07:58 -0400

    Update

--- HLS/utils.cpp
+++ HLS/utils.cpp
@@ -1,12 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Course:      ECE8893 - Parallel Programming for FPGAs
 // Filename:    utils.cpp
-// Description: Utility functions to implement tiling-based convolution 
-//
-// Note:        Modify/complete the functions as required by your design. 
-//
-//              You are free to create any additional functions you may need
-//              for your implementation.
+// Description: Utility functions to implement tiling-based transfer learning 
 ///////////////////////////////////////////////////////////////////////////////
 #include "utils.h"
 #include <iostream>
@@ -16,9 +11,6 @@ using namespace std;
 //--------------------------------------------------------------------------
 // Function to load an input tile block from from off-chip DRAM 
 // to on-chip BRAM.
-//
-// TODO: This is an incomplete function that you need to modify  
-//       to handle the border conditions appropriately while loading.
 //--------------------------------------------------------------------------
 void load_input_tile_block_from_DRAM (
     fm_t in_fm_buf[IN_BUF_DEPTH][IN_BUF_HEIGHT][IN_BUF_WIDTH], 
@@ -48,11 +40,6 @@ void load_input_tile_block_from_DRAM (
                 } else {
                     in_fm_buf[c][i][j] = in_fm[c][height_offset + i - P][width_offset + j - P]; 
                 }
-                // TODO: Handle border features here
-                //
-                // Hint: Either load 0 or input feature into 
-                //       the buffer based on border conditions
-                //in_fm_buf[c][i][j] = 0; // Just a placeholder
             }
         }
     }
@@ -60,45 +47,7 @@ void load_input_tile_block_from_DRAM (
 
 //--------------------------------------------------------------------------
 // Function to load layer parameters (weights and bias) for convolution.
-//
-// This function can be used as-is in your design. However, you are free to
-// add pragmas, restructure code, etc. depending on your way of optimization.
 //--------------------------------------------------------------------------
-/*void load_layer_params_from_DRAM(
-    wt_t weight_buf[OUT_BUF_DEPTH][IN_BUF_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
-    wt_t bias_buf[OUT_BUF_DEPTH],
-    wt_t weights[OUT_FM_DEPTH][IN_FM_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
-    wt_t bias[OUT_FM_DEPTH],
-    int  kernel_group
-)
-{
-    const int kernel_offset  = kernel_group * OUT_BUF_DEPTH;
-
-    WEIGHT_KERNEL_NUM:
-    for(int f = 0; f < OUT_BUF_DEPTH; f++)
-    {
-        WEIGHT_KERNEL_DEPTH:
-        for(int c = 0; c < IN_BUF_DEPTH; c++)
-        {
-            WEIGHT_KERNEL_HEIGHT:
-            for(int kh = 0; kh < 7; kh++)
-	        {
-                WEIGHT_KERNEL_WIDTH:
-	            for(int kw = 0; kw < 7; kw++)
-	            {
-	                weight_buf[f][c][kh][kw] = weights[kernel_offset + f][c][kh][kw];
-                }
-            }
-        }
-    }
-    
-    BIAS:
-    for(int f = 0; f < OUT_BUF_DEPTH; f++)
-    {
-        bias_buf[f] = bias[kernel_offset + f];
-    }
-
-}*/
 
 void load_layer_params_from_DRAM(
     wt_t weight_buf[OUT_BUF_DEPTH][IN_BUF_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
@@ -131,45 +80,7 @@ WEIGHT_KERNEL_NUM:
 // Function to store complete output tile block from BRAM to DRAM.
 //
 // In-place ReLU has been incorporated for convenience.
-//
-// This function can be used as-is in your design. However, you are free to
-// add pragmas, restructure code, etc. depending on your way of optimization.
 //--------------------------------------------------------------------------
-/*void store_output_tile_to_DRAM(
-    fm_t conv_out_fm[OUT_CONV_FM_DEPTH][OUT_CONV_FM_HEIGHT][OUT_CONV_FM_WIDTH],
-    fm_t out_fm_buf[OUT_BUF_DEPTH][OUT_BUF_HEIGHT][OUT_BUF_WIDTH], 
-    int  ti,
-    int  tj,
-    int  kernel_group
-)
-{
-    const int depth_offset  = kernel_group * OUT_BUF_DEPTH;
-    const int height_offset = ti * OUT_BUF_HEIGHT;
-    const int width_offset  = tj * OUT_BUF_WIDTH;
-
-    OUTPUT_BUFFER_DEPTH:
-    for(int f = 0; f < OUT_BUF_DEPTH; f++)
-    {
-        OUTPUT_BUFFER_HEIGHT:
-        for(int i = 0; i < OUT_BUF_HEIGHT; i++)
-        {
-            OUTPUT_BUFFER_WIDTH:
-            for(int j = 0; j < OUT_BUF_WIDTH; j++)
-            {
-                // ReLU in-place
-                //if (out_fm_buf[f][i][j] < (fm_t)0)
-                //{
-                //    out_fm[depth_offset + f][height_offset + i][width_offset + j] = (fm_t) 0;
-                //}
-                //else
-                //{
-                //    out_fm[depth_offset + f][height_offset + i][width_offset + j] = out_fm_buf[f][i][j];
-                //}
-                conv_out_fm[depth_offset + f][height_offset + i][width_offset + j] = out_fm_buf[f][i][j];
-            }
-        }
-    }
-}*/
 
 void store_output_tile_to_DRAM(
     fm_t conv_out_fm[OUT_MAX_POOL_FM_DEPTH][OUT_MAX_POOL_FM_HEIGHT][OUT_MAX_POOL_FM_WIDTH],
@@ -192,18 +103,8 @@ OUTPUT_BUFFER_DEPTH:
         OUTPUT_BUFFER_WIDTH:
             for (int j = 0; j < OUT_MAX_POOL_BUF_WIDTH; j++)
             {
-                // ReLU in-place
-                /*if (out_fm_buf[f][i][j] < (fm_t)0)
-                {
-                    out_fm[depth_offset + f][height_offset + i][width_offset + j] = (fm_t) 0;
-                }
-                else
-                {
-                    out_fm[depth_offset + f][height_offset + i][width_offset + j] = out_fm_buf[f][i][j];
-                }*/
                 conv_out_fm[depth_offset + f][height_offset + i][width_offset + j] = out_fm_buf[f][i][j];
-            	//cout<<conv_out_fm[depth_offset + f][height_offset + i][width_offset + j]<<endl;
-	    }
+	        }
         }
     }
 }
--- HLS/utils.h
+++ HLS/utils.h
@@ -2,9 +2,8 @@
 // Course:      ECE8893 - Parallel Programming for FPGAs
 // Filename:    utils.h
 // Description: Header file for utility functions.
-//
-// You are free to modify the existing functions or add new ones.
 ///////////////////////////////////////////////////////////////////////////////
+
 #include "conv.h"
 
 //--------------------------------------------------------------------------
@@ -17,26 +16,11 @@ void load_input_tile_block_from_DRAM (
     int  tj 
 );
 
-/*void load_layer_params_from_DRAM(
-    wt_t weight_buf[OUT_BUF_DEPTH][IN_BUF_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
-    wt_t bias_buf[OUT_BUF_DEPTH],
-    wt_t weights[OUT_FM_DEPTH][IN_FM_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
-    wt_t bias[OUT_FM_DEPTH],
-    int  kernel_group
-);*/
-
 void load_layer_params_from_DRAM(
     wt_t weight_buf[OUT_BUF_DEPTH][IN_BUF_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
     wt_t weights[OUT_CONV_FM_DEPTH][IN_FM_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
     int  kernel_group
 );
-
-/*void conv_7x7(
-    fm_t Y_buf[OUT_BUF_DEPTH][OUT_BUF_HEIGHT][OUT_BUF_WIDTH], 
-    fm_t X_buf[IN_BUF_DEPTH][IN_BUF_HEIGHT][IN_BUF_WIDTH],
-    wt_t W_buf[OUT_BUF_DEPTH][IN_BUF_DEPTH][KERNEL_HEIGHT][KERNEL_WIDTH],
-    wt_t B_buf[OUT_BUF_DEPTH]
-);*/
 
 void conv_5x5(
     fm_t Y_buf[OUT_BUF_DEPTH][OUT_BUF_HEIGHT][OUT_BUF_WIDTH],
@@ -70,4 +54,22 @@ void linear_layer(
     fm_t linear_input[OUT_MAX_POOL_FM_DEPTH * OUT_MAX_POOL_FM_HEIGHT * OUT_MAX_POOL_FM_WIDTH],
     wt_t linear_weights[IN_LINEAR_LENGTH][OUT_LINEAR_LENGTH],
     fm_t output_feature_map[OUT_LINEAR_LENGTH]
+);
+
+void softmax(
+    fm_t output_feature_map[OUT_LINEAR_LENGTH],
+    fm_t softmax_output[OUT_LINEAR_LENGTH]
+);
+
+void backprop(
+    fm_t linear_input[OUT_MAX_POOL_FM_DEPTH * OUT_MAX_POOL_FM_HEIGHT * OUT_MAX_POOL_FM_WIDTH],
+    wt_t linear_weights[IN_LINEAR_LENGTH][OUT_LINEAR_LENGTH],
+    fm_t output_feature_map[OUT_LINEAR_LENGTH], //This is output of softmax
+    fm_t target_output[OUT_LINEAR_LENGTH]
+);
+
+void calculateMSE(
+    fm_t target_output[OUT_LINEAR_LENGTH],
+    fm_t output_feature_map[OUT_LINEAR_LENGTH],
+    fm_t mse[1]
 );
